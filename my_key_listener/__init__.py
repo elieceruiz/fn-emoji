25fc0405b9fd353b782803c3ca992db09167bdba
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2025-09-29 13:33:44 -0500

    Create __init__.py

---
+++ my_key_listener/__init__.py
@@ -0,0 +1,10 @@
+import os
+import streamlit.components.v1 as components
+
+_component_func = components.declare_component(
+    "my_key_listener",
+    path=os.path.join(os.path.dirname(__file__), "frontend/build")
+)
+
+def my_key_listener(default=None, key=None):
+    return _component_func(default=default, key=key)
