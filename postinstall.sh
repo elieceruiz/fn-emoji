f1a7086ad381c1811838adaaa34677fe4342a9eb
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2025-09-29 15:35:21 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Entrar al frontend y construir React
+echo ">>> Entrando al frontend y construyendo React"
 cd my_key_listener/frontend
 npm install
 npm run build
+echo ">>> Build terminado, carpeta dist creada"
