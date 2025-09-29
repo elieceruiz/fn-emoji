e7bc2213f9a7527124e48de07e399cc508100764
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2025-09-29 16:52:58 -0500

    Update __init__.py

--- my_key_listener/__init__.py
+++ my_key_listener/__init__.py
@@ -1,10 +1,9 @@
-# my_key_listener/__init__.py
 import os
 import streamlit.components.v1 as components
 
 _component_func = components.declare_component(
     "my_key_listener",
-    path=os.path.join(os.path.dirname(__file__), "frontend/dist")  # apunta al build de React
+    path=os.path.join(os.path.dirname(__file__), "frontend", "dist")
 )
 
 def my_key_listener(default=None, key=None):
